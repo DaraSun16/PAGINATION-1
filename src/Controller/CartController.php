7bd8fdf88599f3f4ab4313c2439126c113523f22
Eiculenna-18 <anne-lucie.joinot@hotmail.fr>
2025-06-26 13:04:40 +0200

    Update

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Articles;
 use App\Repository\ArticlesRepository;
+use App\Services\CartServices;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,48 +13,51 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 #[Route('/cart', name: 'app_')] // This route prefix will be used for all routes in this controller
 final class CartController extends AbstractController
 {
+    public function __construct( private CartServices $cartServices ) 
+    { 
+        #--#
+    }
+
     // ---------- ADD PRODUCT TO PANIER -----------------
     #[Route('/cart/{id}', name: 'cart')] // This route will match /cart/{id} and use the name 'app_cart' with concatenated prefix
-    public function add(Articles $articles, SessionInterface $session): Response
+    public function add(Articles $articles): Response // + SessionInterface $session dans les parantheses
     {
-        $id = $articles->getId();
+        // $id = $articles->getId();
+        // $panier = $this->getSession()->get('panier', []);
+        // if ( empty($panier[$id])){ 
+        //     $panier[$id] = 1; // 1 = quantity add 1 by 1
+        // } else {
+        //     $panier[$id]++; 
+        // }
+        // // $panier[$id] = ( empty ($pannier[$id]) ? 1 : $panier[$id]+1 ); en terme ternaire
+        // $this->getSession()->set('panier', $panier);
+        // Deplacer dans CartServices.php
 
-        $panier = $session->get('panier', []);
-
-        if ( empty($panier[$id])){
-            $panier[$id] = 1; // 1 = quantity add 1 by 1
-        } else {
-            $panier[$id]++; 
-        }
-        // $panier[$id] = ( empty ($pannier[$id]) ? 1 : $panier[$id]+1 ); en terme ternaire
-
-        $session->set('panier', $panier);
+        $this->cartServices->add($articles);
 
         return $this->redirectToRoute('app_index');
     }
 
     // ---------- TOTAL PANIER -----------------
     #[Route('/', name: 'index')]
-    public function index(SessionInterface $session, ArticlesRepository $articlesRepository)
+    public function index() // SessionInterface $session, ArticlesRepository $articlesRepository entre parantheses
     {
-        $panier = $session->get('panier', []); // Retrieve session panier
-
-        $data = [];
-        $total = 0;
-
-        foreach ($panier as $key => $quantity) { //$id if promblem
-            $article = $articlesRepository->find($key); //$id if promblem
-            $data[] = [
-                'article' => $article,
-                'quantity' => $quantity,
-            ];
-
-            $total += $article->getPrice() * $quantity;
-        }
+        // $panier = $session->get('panier', []); // Retrieve session panier
+        // $data = [];
+        // $total = 0;
+        // foreach ($panier as $key => $quantity) { //$id if promblem
+        //     $article = $articlesRepository->find($key); //$id if promblem
+        //     $data[] = [
+        //         'article' => $article,
+        //         'quantity' => $quantity,
+        //     ];
+        //     $total += $article->getPrice() * $quantity;
+        // }
+        // Deplacer dans CartServices.php
 
         return $this->render('cart/cart.html.twig', [
-            'data' => $data,
-            'total' => $total,
+            'data' => $this->cartServices->getFullcart(), // $data à la place de $this
+            'total' => $this->cartServices->getTotal(), // $total à la place de $this
         ]);
     }
 
@@ -61,20 +65,20 @@ final class CartController extends AbstractController
     #[Route('/remove/{id}', name: 'remove')]
     public function remove(Articles $articles, SessionInterface $session)
     {
-        $id = $articles->getId();
+        // $id = $articles->getId();
+        // $panier = $session->get('panier', []);
+        // if ( !empty($panier[$id]) ) {
+        //     if ( $panier[$id] > 1 ) {
+        //         $panier[$id]--;
+        //     } else {
+        //         //mettre un overlay pour être sur que le client ne veuille plus du produit
+        //         unset( $panier[$id] );
+        //     }
+        // }
+        // $session->set('panier', $panier);
+        // déplacer dans CartServices.php
 
-        $panier = $session->get('panier', []);
-
-        if ( !empty($panier[$id]) ) {
-            if ( $panier[$id] > 1 ) {
-                $panier[$id]--;
-            } else {
-                //mettre un overlay pour être sur que le client ne veuille plus du produit
-                unset( $panier[$id] );
-            }
-        }
-
-        $session->set('panier', $panier);
+        $this->cartServices->remove($articles); // ligne ajouter
 
         return $this->redirectToRoute('app_index');
     }
@@ -83,15 +87,19 @@ final class CartController extends AbstractController
     #[Route('/delete/{id}', name: 'delete')]
     public function delete(Articles $articles, SessionInterface $session)
     {
-        $id = $articles->getId();
+        // $id = $articles->getId();
 
-        $panier = $session->get('panier', []);
+        // $panier = $session->get('panier', []);
 
-        if ( !empty($panier[$id]) ) {
-            unset( $panier[$id] );
-        }
+        // if ( !empty($panier[$id]) ) {
+        //     unset( $panier[$id] );
+        // }
 
-        $session->set('panier', $panier);
+        // $session->set('panier', $panier);
+        // Deplacer dans CartServices.php
+
+        $this->cartServices->delete($articles); // ligne ajouter
+
         return $this->redirectToRoute('app_index');
     }
 
@@ -100,6 +108,7 @@ final class CartController extends AbstractController
     public function empty(SessionInterface $session)
     {
         $session->remove('panier');
+
         return $this->redirectToRoute('app_index');
     }
 }
--- src/Services/CartServices.php
+++ src/Services/CartServices.php
@@ -2,11 +2,13 @@
 
 namespace App\Services;
 
+use App\Entity\Articles;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class CartServices
 {
+    // ---------- CONSTRUCT SESSION -----------------
     public function __construct(
         private RequestStack $requestStack,
         private EntityManagerInterface $entityManager
@@ -20,8 +22,88 @@ class CartServices
         return $this->requestStack->getSession();
     }
 
-    public function add()
+    // ---------- ADD PRODUCT TO PANIER -----------------
+    public function add(Articles $articles)
     {
-        return $this->requestStack->getSession();
+        $id = $articles->getId();
+
+        $panier = $this->getSession()->get('panier', []);
+
+        if ( empty($panier[$id])){
+            $panier[$id] = 1; 
+        } else {
+            $panier[$id]++; 
+        }
+
+        $this->getSession()->set('panier', $panier);
     }
+
+    // ---------- TOTAL PANIER 1/2 -----------------
+    public function getFullcart()
+    {
+        $panier = $this->getSession()->get('panier', []); 
+
+        $data = [];
+        $total = 0;
+
+        foreach ($panier as $key => $quantity) { 
+
+            $article = $this->entityManager->getRepository(Articles::class)->find($key); 
+            
+            $data[] = [
+                'article' => $article,
+                'quantity' => $quantity,
+            ];
+        }
+
+        return $data;
+    }
+
+    // ---------- TOTAL PANIER 2/2 -----------------
+    public function getTotal()
+    {
+        $total = 0;
+
+        foreach ( $this->getFullcart() as $item){
+            $totalItem = $item['article']->getPrice() * $item['quantity'];
+            $total += $totalItem;
+        }
+
+        return $total;
+    }
+
+    // ---------- REMOVE PRODUCT TO PANIER -----------------
+    public function remove(Articles $articles)
+    {
+        $id = $articles->getId();
+
+        $panier = $this->getSession()->get('panier', []);
+
+        if ( !empty($panier[$id]) ) {
+            if ( $panier[$id] > 1 ) {
+                $panier[$id]--;
+            } else {
+                unset( $panier[$id] );
+            }
+        }
+
+        $this->getSession()->set('panier', $panier);
+    }
+
+    // ---------- DELETE LINE PRODUCT TO PANIER -----------------
+    public function delete(Articles $articles)
+    {
+        $id = $articles->getId();
+
+        $panier = $this->getSession()->get('panier', []);
+
+        if ( !empty($panier[$id]) ) {
+            unset( $panier[$id] );
+        }
+
+        $this->getSession()->set('panier', $panier);
+    }
+
+    
+    
 }
