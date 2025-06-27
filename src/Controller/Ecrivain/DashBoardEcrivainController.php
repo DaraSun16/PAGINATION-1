11916b30f3367ed966d4a02b8110b92040a7e6c6
Eiculenna-18 <anne-lucie.joinot@hotmail.fr>
2025-06-27 10:58:51 +0200

    Update Procttion DashBoard User

--- src/Controller/Ecrivain/DashBoardEcrivainController.php
+++ src/Controller/Ecrivain/DashBoardEcrivainController.php
@@ -8,7 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class DashBoardEcrivainController extends AbstractController
 {
-    #[Route('/dash/board/ecrivain', name: 'app_dash_board_ecrivain')]
+    #[Route('/dash/board/ecrivain', name: 'app_dash_board_ecrivain')] //1ere protection
+    // #[IsGranted('ROLE_ECRIVAIN', message: 'Je dois être connecté pour accéder à la page.', statusCode: 404, exceptionCode: 404)] //2e protection
     public function index(): Response
     {
         return $this->render('ecrivain/dash_board_ecrivain/index.html.twig', [
--- src/Controller/Profile/DashBoardProfileController.php
+++ src/Controller/Profile/DashBoardProfileController.php
@@ -5,10 +5,12 @@ namespace App\Controller\Profile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class DashBoardProfileController extends AbstractController
 {
-    #[Route('/dash/board/profile', name: 'app_dash_board_profile')]
+    #[Route('/dash/board/profile', name: 'app_dash_board_profile')] //1ere protection
+    #[IsGranted('ROLE_USER', message: 'Je dois être connecté pour accéder à la page.', statusCode: 404, exceptionCode: 404)] //2e protection
     public function index(): Response
     {
         return $this->render('profile/dash_board_profile/index.html.twig', [
